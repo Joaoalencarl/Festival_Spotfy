8d73421359fdd91944850120e62772334d55e3bb
joaoalencarl <jaal.profissional@gmail.com>
2023-05-16 11:13:58 -0300

    modified:   .cache 	modified:   .env 	new file:   .vscode/launch.json 	modified:   spotfy_api.py

--- spotfy_api.py
+++ spotfy_api.py
@@ -4,15 +4,17 @@ import os
 from dotenv import load_dotenv
 from spotipy.oauth2 import SpotifyOAuth
 
-CLIENT_ID = os.getenv("CLIENT_ID")
-CLIENT_SECRET = os.getenv("CLIENT_SECRET")
-REDIRECT_URI = os.getenv("REDIRECT_URI")
+load_dotenv()
+
+CLIENT_ID = "a481aa2eb31248c39a5ea009c28c1d9d"
+CLIENT_SECRET = "b1a9a9e2c64c41839a1126e8aa722ee6"
+REDIRECT_URI = "http://localhost:8000"
 
 # Configuraçãod
 scope = "playlist-modify-public"
 
-sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id="CLIENT_ID",
-                     client_secret="CLIENT_SECRET", redirect_uri="REDIRECT_URI", scope=scope))
+sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=CLIENT_ID,
+                     client_secret=CLIENT_SECRET, redirect_uri=REDIRECT_URI, scope=scope))
 
 
 # Coletando as informações da playlist
