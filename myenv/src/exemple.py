447e4431cd8f4ba920b5287d138b2777c2a547d7
Joaoalencarl <jaal.profissional@gmail.com>
2023-05-23 14:25:41 -0300

    mvp project

--- myenv/src/exemple.py
+++ myenv/src/exemple.py
@@ -8,25 +8,23 @@ load_dotenv()
 
 class SpotifyClient:
     def __init__(self, client_id, client_secret, redirect_uri):
-        self.client_id = client_id
-        self.client_secret = client_secret
-        self.redirect_uri = redirect_uri
+        self.__client_id = client_id
+        self.__client_secret = client_secret
+        self.__redirect_uri = redirect_uri
+        self.sp = None
 
     def authenticate(self, scope):
-        return spotipy.Spotify(
+        self.sp = spotipy.Spotify(
             auth_manager=SpotifyOAuth(
-                client_id=self.client_id,
-                client_secret=self.client_secret,
-                redirect_uri=self.redirect_uri,
+                client_id=self.__client_id,
+                client_secret=self.__client_secret,
+                redirect_uri=self.__redirect_uri,
                 scope=scope
             )
         )
 
 
-class PlaylistManager:
-    def __init__(self, sp):
-        self.sp = sp
-
+class PlaylistManager(SpotifyClient):
     def create_playlist(self, user, name, description, public):
         return self.sp.user_playlist_create(
             user=user,
@@ -39,15 +37,7 @@ class PlaylistManager:
         self.sp.playlist_add_items(playlist_id, track_uris)
 
 
-class EventPlaylistCreator:
-    def __init__(self, client_id, client_secret, redirect_uri):
-        self.spotify_client = SpotifyClient(client_id, client_secret, redirect_uri)
-        self.playlist_manager = None
-
-    def authenticate_spotify(self, scope):
-        sp = self.spotify_client.authenticate(scope)
-        self.playlist_manager = PlaylistManager(sp)
-
+class EventPlaylistCreator(PlaylistManager):
     def collect_artist_information(self):
         artists = input("Digite o nome dos artistas (separados por vírgula): ").split(',')
         return artists
@@ -55,7 +45,7 @@ class EventPlaylistCreator:
     def create_event_playlist(self, user, playlist_name, event_day):
         artists = self.collect_artist_information()
 
-        playlist = self.playlist_manager.create_playlist(
+        playlist = self.create_playlist(
             user=user,
             name=playlist_name,
             description=f'Esquenta! o {playlist_name} é dia {event_day}',
@@ -64,12 +54,12 @@ class EventPlaylistCreator:
         playlist_id = playlist['id']
 
         for artist in artists:
-            results = self.playlist_manager.sp.search(q='artist:' + artist, type='artist', limit=1)
+            results = self.sp.search(q='artist:' + artist, type='artist', limit=1)
             if len(results['artists']['items']) > 0:
-                artist_id = results['artists']['items'][0]['id']['BR']
-                top_tracks = self.playlist_manager.sp.artist_top_tracks(artist_id)
+                artist_id = results['artists']['items'][0]['id']
+                top_tracks = self.sp.artist_top_tracks(artist_id)
                 track_uris = [track['uri'] for track in top_tracks['tracks'][:10]]
-                self.playlist_manager.add_tracks_to_playlist(playlist_id, track_uris)
+                self.add_tracks_to_playlist(playlist_id, track_uris)
 
         playlist_url = f"https://open.spotify.com/playlist/{playlist_id}"
         print("Link da playlist:", playlist_url)
@@ -82,7 +72,7 @@ if __name__ == "__main__":
     redirect_uri = os.getenv('REDIRECT_URI')
 
     event_playlist_creator = EventPlaylistCreator(client_id, client_secret, redirect_uri)
-    event_playlist_creator.authenticate_spotify('playlist-modify-public')
+    event_playlist_creator.authenticate('playlist-modify-public')
 
     username = input("Digite o nome do usuário: ")
     playlist_name = input("Digite o nome do seu próximo evento: ")
